8a0df3d85508c5e9b23cd3980a7818ee9ad1022d
moasko.dev <42101671+moasko@users.noreply.github.com>
2026-05-05 00:27:01 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import betterlytics from "@betterlytics/tracker"
+
+betterlytics.init("expo-assets-studio-morvzxm1")
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
